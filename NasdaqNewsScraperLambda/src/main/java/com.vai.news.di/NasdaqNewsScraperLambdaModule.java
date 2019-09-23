4e39cdb39ab5579df11440ea2f41158f5d8252da
Vaibhav Walia <walia.vaibhav@yahoo.com>
2019-09-23 00:09:06 -0700

    Adds capability to publish notifications to sns topic

--- NasdaqNewsScraperLambda/src/main/java/com.vai.news.di/NasdaqNewsScraperLambdaModule.java
+++ NasdaqNewsScraperLambda/src/main/java/com.vai.news.di/NasdaqNewsScraperLambdaModule.java
@@ -1,22 +1,29 @@
 package com.vai.news.di;
 
 import javax.inject.Singleton;
+import javax.inject.Named;
+
 import dagger.Module;
 import dagger.Provides;
 
 import lombok.NonNull;
 import okhttp3.OkHttpClient;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.vai.news.util.TickerProvider;
 import com.vai.news.util.NasdaqScraper;
 import com.vai.news.util.NotificationPublisher;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 /**
  * Dagger module which provides dependencies for the NasdaqNewsScraperLambda.
  */
 @Module
 public class NasdaqNewsScraperLambdaModule {
+
+    private static final String NOTIFICATION_TOPIC_ARN = "NOTIFICATION_TOPIC_ARN";
 
     @Singleton
     @Provides
@@ -32,8 +39,10 @@ public class NasdaqNewsScraperLambdaModule {
 
     @Singleton
     @Provides
-    NotificationPublisher provideNotificationPublisher() {
-        return new NotificationPublisher();
+    NotificationPublisher provideNotificationPublisher(@NonNull final AmazonSNS amazonSNS,
+                                                       @NonNull final ObjectMapper mapper,
+                                                       @NonNull @Named(NOTIFICATION_TOPIC_ARN) final String arn) {
+        return new NotificationPublisher(amazonSNS, mapper, arn);
     }
 
     @Singleton
@@ -44,8 +53,21 @@ public class NasdaqNewsScraperLambdaModule {
 
     @Singleton
     @Provides
-    ObjectMapper  provideObjectMapper() {
+    ObjectMapper provideObjectMapper() {
         return new ObjectMapper();
+    }
+
+    @Singleton
+    @Provides
+    AmazonSNS provideAmazonSNS() {
+        return AmazonSNSClientBuilder.defaultClient();
+    }
+
+    @Singleton
+    @Provides
+    @Named(NOTIFICATION_TOPIC_ARN)
+    String provideNotificationTopicArn() {
+        return System.getenv(NOTIFICATION_TOPIC_ARN);
     }
 
 }
--- NasdaqNewsScraperLambda/src/main/java/com.vai.news.util/NotificationPublisher.java
+++ NasdaqNewsScraperLambda/src/main/java/com.vai.news.util/NotificationPublisher.java
@@ -1,21 +1,37 @@
 package com.vai.news.util;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.vai.news.models.NewsNotification;
 
 /**
  * This module is responsible for publishing NewsNotifications.
  */
+@AllArgsConstructor
 @Log4j2
 public class NotificationPublisher {
+
+    private final AmazonSNS amazonSNS;
+
+    private final ObjectMapper mapper;
+
+    private final String notificationTopicArn;
 
     /**
      * Given a NewsNotification, logs it to the console.
      * @param notification
      */
+    @SneakyThrows
     public void publish(@NonNull final NewsNotification notification) {
-        log.info("Published news notification: {}", notification);
+        final PublishResult publishResult = amazonSNS.publish(notificationTopicArn, mapper.writeValueAsString(notification));
+        log.info("Published news notification : {}", publishResult);
     }
 }
